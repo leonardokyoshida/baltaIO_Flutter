26d81c1eefec20c6c2ac010068e2b759aabf0378
Leonardo Kenji Yoshida <yoshidak.leonardo@outlook.com>
2020-01-06 01:00:31 -0300

    ListView, created items, TextFormField(new task)

--- firstApp/todo/lib/main.dart
+++ firstApp/todo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/item.dart';
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -16,17 +18,54 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+  HomePage() {
+    items = [];
+
+    items.add(Item(title: "item 1", done: false));
+    items.add(Item(title: "item 2", done: true));
+    items.add(Item(title: "item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo list"),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Ola Mundo"),
+          title: TextFormField(
+        controller: newTaskCtrl,
+        keyboardType: TextInputType.text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
         ),
+        decoration: InputDecoration(
+          labelText: "Nova tarefa",
+          labelStyle: TextStyle(color: Colors.white),
+        ),
+      )),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (context, index) {
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {
+              setState(() {
+                item.done = value;
+              });
+            },
+          );
+        },
       ),
     );
   }

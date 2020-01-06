0a2977e29ddcb02871f49bc2158c5a94b2efdd70
Leonardo Kenji Yoshida <yoshidak.leonardo@outlook.com>
2020-01-06 00:25:38 -0300

    Initial commit

---
+++ firstApp/todo/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODO APP',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo list"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Ola Mundo"),
+        ),
+      ),
+    );
+  }
+}
